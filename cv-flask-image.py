ba8fa39a2c3b4e9ed32badc930d306e1d6b329ea
yangyou <onbug@qq.com>
2018-11-16 15:39:08 +0800

    add flask static file

--- cv-flask-image.py
+++ cv-flask-image.py
@@ -12,6 +12,8 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
+photoFolder = os.path.dirname(os.path.realpath(__file__))
+
 
 @app.route("/test")
 def test():
@@ -28,16 +30,49 @@ def filephoto():
         
 @app.route("/photo")
 def photo():
-        result = {}
-        for i in range(2):
-            ret, frame = cap.read()
+    result = {}
+    for i in range(2):
+        ret, frame = cap.read()
+    if ret:
+        ret, jpgcontent = cv2.imencode('.jpeg', frame)
         if ret:
-            ret, jpgcontent = cv2.imencode('.jpeg', frame)
-            if ret:
-                resp = Response(jpgcontent.tostring(), content_type="image/jpeg")
-                return resp
-            result = {'res': 'failed'}
+            resp = Response(jpgcontent.tostring(), content_type="image/jpeg")
+            return resp
+        result = {'res': 'failed'}
 
+    return jsonify(result)
+
+def photo_n():
+    result = {'res': 'failed'}
+    try:
+        camid = request.args.get('cam', default = 0, type = int)
+        print 'camid: ' + str(camid)
+        cap = capMap[camid]
+        for i in range(5):
+            ret, frame = cap.read()
+        if ret:    
+            nowYearMonthDay = time.strftime("%Y%m%d")
+            parentPath = os.path.join(photoFolder, nowYearMonthDay)
+            print('parentpath: ', parentPath)
+            if not os.path.exists(parentPath):
+                os.mkdir(parentPath)
+            nowtimestampString = time.strftime("%Y%m%d%H%M%S")
+            filename = nowtimestampString + '_{camid}.jpg'.format(camid=camid)
+            fullpath = os.path.join(parentPath, filename)
+            cv2.imwrite(fullpath, frame)
+            ret, jpgcontent = cv2.imencode('.jpg', frame)
+            if ret:
+                command = 'ls {parentPath}/*_{camid}.jpg | wc -l'.format(parentPath=parentPath, camid=camid)
+                pic_number = os.popen(command).read()
+                pic_number = pic_number.strip()
+
+                base64_content = base64.b64encode(jpgcontent)
+                imgdata = 'data:image/jpeg;base64,' + base64_content
+
+                return render_template('index.html', pic_number=pic_number, imgdata = imgdata)       
+            return jsonify(result)
+    except Exception as e:
+        print e
         return jsonify(result)
 
 if __name__ == '__main__':
--- flask-server.py
+++ flask-server.py
@@ -1,24 +1,39 @@
 #coding: utf8
+import os
+import base64
 from flask import Flask
-from flask import Response, jsonify
+from flask import Response, jsonify, request, render_template
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 
 monkey.patch_all()
 
-app = Flask(__name__)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+app = Flask(__name__, static_url_path=dir_path)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World! from gbot pic server"
+    print 'get index'
+    f = open('/Users/yangyou/Downloads/zkb-2.jpg')
+    content = f.read()
+    base64_content = base64.b64encode(content)
+    imgdata = 'data:image/jpeg;base64,' + base64_content
+    return render_template('index.html', pic_number=3, imgdata = imgdata)
 
+
+# /photo?cam=3
 @app.route("/photo")
 def photo():
     result = {}
+    camid = request.args.get('cam', default = 1, type = int)
     result['code'] = 10000
+    result['cam'] = camid
     return jsonify(result)
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8088)
-    http_server = WSGIServer(('', 8088), app)
+    
+    port = 8088
+    http_server = WSGIServer(('', port), app)
+    print 'listening on: %d'%(port)
     http_server.serve_forever()
