76fbc23551c456c8e7b9a30ad94cd0e6f33aa30f
yangyou <onbug@qq.com>
2019-06-11 11:40:45 +0800

    add manager to multiprocess

--- multiprocess.py
+++ multiprocess.py
@@ -1,6 +1,6 @@
 import time
 import multiprocessing
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Manager
 
 CPU_NUM = multiprocessing.cpu_count()
 
@@ -18,9 +18,47 @@ class Task():
         self.proc.start()
     
 
-taskList = []
-for i in range(CPU_NUM):
-    task = Task(i)
-    task.start()
-    taskList.append(task)
+# taskList = []
+# for i in range(CPU_NUM):
+#     task = Task(i)
+#     task.start()
+#     taskList.append(task)
 
+
+container = Queue(maxsize=10)
+
+
+def processPro(gvalue):
+    while True:
+        if container.full():
+            
+            ele = container.get()
+            print('pro', ele)
+        current = time.time()
+        container.put(current)
+        gvalue['time'] = current
+        time.sleep(0.1)
+
+
+def processCon(gvalue, no):
+    while True:
+        ele = container.get()
+        print(no, ele)
+        print("{no}, current: {current}".format(no=no, current=gvalue['time']))
+        time.sleep(1)
+
+def main():
+    gValue = Manager().dict({'time': 0})
+    pro = Process(target=processPro, args=(gValue,))
+    pro.start()
+
+    for i in range(5):
+        pro = Process(target=processCon, args=(gValue, i))
+        pro.start()
+    
+    while True:
+        print('main: ', gValue['time'])
+        time.sleep(2)
+
+
+main()
