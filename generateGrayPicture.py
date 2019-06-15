e91fe7a33928bd3759e54ea002aa39b3583e949c
buggoing <tjbuggo@gmail.com>
2019-06-15 22:40:59 +0800

    add os.path

--- generateGrayPicture.py
+++ generateGrayPicture.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python    
 # encoding: utf-8
-import sys 
+import sys
+import os
 from PIL import Image
 import cv2
 
-def main(filename)
-
-  img = cv2.imread(filename)
+def main(filepath):
+  basename = os.path.basename(filepath)
+  dirname = os.path.dirname(filepath)
+  img = cv2.imread(filepath)
   grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  cv2.imwrite('gray-' + filename, grayImg)
+  # grayImg = img.convert('L')
+  newFileName = 'grey_' + basename
+  newFilepath = os.path.join(dirname, newFileName)
+  print(newFileName, newFilepath)
+  cv2.imwrite(newFilepath, grayImg)
   cv2.imshow('gray', grayImg)
   
   # img = Image.open(filename)
